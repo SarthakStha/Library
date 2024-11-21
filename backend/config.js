9a54f4a1ac8b2ac39e5e15cefb872c6c4c1d28f7
Shrestha, Sarthak <sarthak3926@gmail.com>
2024-11-21 10:11:01 -0600

    MongoDB connected with url

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://root:root@library.qzwyc.mongodb.net/?retryWrites=true&w=majority&appName=Library";
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 // importing all the necessay folders
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 // creating an instance of an expression applicaiton
 const app = express();
 
-// used to request data form the server
-app.get();
+//method used to request data form the server
+//
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome to the MERN stack Tutorial');
+});
 
 /*
 Here the second parameter after the port is a callback funciton
@@ -14,6 +19,16 @@ callback function is a function which is passed as a parameter which
 executes after the main function, it allows the user to handle asynchronous
 operations 
 */
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
-});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to the database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+        
+    });
